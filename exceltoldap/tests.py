82b6e1209ee261bc4750dc9a7ac8c85f71dfce4b
Tobias Carlander <tobias.carlander@wfp.org>
2012-10-21 10:11:48 +0400

    Adding start testcase for vehicle view

--- exceltoldap/tests.py
+++ exceltoldap/tests.py
@@ -6,4 +6,9 @@ class ExcelViewTestCase(TestCase):
         resp = self.client.get('/users/')
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('users' in resp.context)
+    def test_vheicles(self):
+        resp = self.client.get('/vehicles/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('vehicles' in resp.context)
+        
         
