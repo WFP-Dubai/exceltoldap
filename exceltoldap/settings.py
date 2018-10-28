f061d727b9870977ad1a72ac7f5674aacf3906ca
Mohammed Kaleem <kaleem6110@gmail.com>
2018-10-28 12:24:54 +0400

    Update settings.py

--- exceltoldap/settings.py
+++ exceltoldap/settings.py
@@ -12,15 +12,15 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'excelldap',                      # Or path to database file if using sqlite3.
-        'USER': 'excelldap',                      # Not used with sqlite3.
-        'PASSWORD': 'excelldap',                  # Not used with sqlite3.
-        'HOST': '10.11.208.35',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     },
     'default_2': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'database.db',                      # Or path to database file if using sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
